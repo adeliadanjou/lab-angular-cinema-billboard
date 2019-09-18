1e881d419ea0b9a1640bb3d010e3b230ad1c00be
adeliadanjou <princessadelia@gmail.com>
2019-09-18 14:09:33 +0200

    home done

--- starter-code/src/app/my-home/my-home.component.ts
+++ starter-code/src/app/my-home/my-home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../services/movies.service'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-my-home',
   templateUrl: './my-home.component.html',
-  styleUrls: ['./my-home.component.css']
+  styleUrls: ['./my-home.component.css'],
+  providers: [MoviesService]
 })
 export class MyHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private moviesService: MoviesService, public router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToMovie(id){
+    this.router.navigate(['movie',id])
   }
 
 }
